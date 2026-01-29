bec740e75ee248893bc1471ca594ac1d3b10271e
yuwen <yuwen@succinct.xyz>
2026-01-29 14:13:46 -0800

    handle errors

--- actions.go
+++ actions.go
@@ -38,11 +38,13 @@ func getPhase1Commons(phase1 *mpcsetup.Phase1) *mpcsetup.SrsCommons {
 }
 
 // computePhase2Hash computes the hash of a Phase2 by serializing and hashing
-func computePhase2Hash(phase2 *mpcsetup.Phase2) []byte {
+func computePhase2Hash(phase2 *mpcsetup.Phase2) ([]byte, error) {
 	var buf bytes.Buffer
-	phase2.WriteTo(&buf)
+	if _, err := phase2.WriteTo(&buf); err != nil {
+		return nil, fmt.Errorf("serializing phase2 for hash: %w", err)
+	}
 	h := sha256.Sum256(buf.Bytes())
-	return h[:]
+	return h[:], nil
 }
 
 func p1i(cCtx *cli.Context) error {
@@ -68,6 +70,7 @@ func p1i(cCtx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	defer outputFile.Close()
 
 	_, err = phase1.WriteTo(outputFile)
 	if err != nil {
@@ -94,7 +97,9 @@ func p2n(cCtx *cli.Context) error {
 	}
 	defer phase1File.Close()
 	phase1 := &mpcsetup.Phase1{}
-	phase1.ReadFrom(phase1File)
+	if _, err := phase1.ReadFrom(phase1File); err != nil {
+		return fmt.Errorf("reading phase1: %w", err)
+	}
 
 	fmt.Println("reading r1cs")
 	r1csFile, err := os.Open(r1csPath)
@@ -103,9 +108,13 @@ func p2n(cCtx *cli.Context) error {
 	}
 	defer r1csFile.Close()
 	r1cs := &cs.R1CS{}
-	r1cs.ReadFrom(r1csFile)
+	if _, err := r1cs.ReadFrom(r1csFile); err != nil {
+		return fmt.Errorf("reading r1cs: %w", err)
+	}
 
-	// Get SrsCommons from Phase1 using reflection
+	// Get SrsCommons from Phase1 using reflection.
+	// We cannot use phase1.Seal(beacon) because Phase1 was imported from an
+	// existing PTAU ceremony and does not need a beacon contribution.
 	commons := getPhase1Commons(phase1)
 
 	fmt.Println("initializing phase2")
@@ -118,7 +127,9 @@ func p2n(cCtx *cli.Context) error {
 		return err
 	}
 	defer phase2File.Close()
-	phase2.WriteTo(phase2File)
+	if _, err := phase2.WriteTo(phase2File); err != nil {
+		return fmt.Errorf("writing phase2: %w", err)
+	}
 
 	// Write evals - serialize manually since Phase2Evaluations doesn't have WriteTo in new API
 	fmt.Println("writing evals")
@@ -183,7 +194,9 @@ func p2c(cCtx *cli.Context) error {
 	}
 	defer inputFile.Close()
 	phase2 := &mpcsetup.Phase2{}
-	phase2.ReadFrom(inputFile)
+	if _, err := phase2.ReadFrom(inputFile); err != nil {
+		return fmt.Errorf("reading phase2: %w", err)
+	}
 
 	fmt.Printf("Generating contribution\n")
 	phase2.Contribute()
@@ -193,7 +206,9 @@ func p2c(cCtx *cli.Context) error {
 		return err
 	}
 	defer outputFile.Close()
-	phase2.WriteTo(outputFile)
+	if _, err := phase2.WriteTo(outputFile); err != nil {
+		return fmt.Errorf("writing phase2: %w", err)
+	}
 
 	fmt.Printf("Uploading contribution: phase2-%d\n", contributionIndex)
 	err = Upload(outputPh2Path, presignedUploadUrl)
@@ -202,7 +217,11 @@ func p2c(cCtx *cli.Context) error {
 	}
 
 	// Compute hash of the contribution
-	hash := hex.EncodeToString(computePhase2Hash(phase2))
+	hashBytes, err := computePhase2Hash(phase2)
+	if err != nil {
+		return err
+	}
+	hash := hex.EncodeToString(hashBytes)
 
 	downloadURL := fmt.Sprintf("http://%s.s3.%s.amazonaws.com/phase2-%d",
 		bucketName,
@@ -244,7 +263,9 @@ func p2v(cCtx *cli.Context) error {
 	}
 	defer inputFile.Close()
 	input := &mpcsetup.Phase2{}
-	input.ReadFrom(inputFile)
+	if _, err := input.ReadFrom(inputFile); err != nil {
+		return fmt.Errorf("reading contribution: %w", err)
+	}
 
 	originKey := "phase2"
 	idx, err := strconv.Atoi(contributionIndex)
@@ -267,9 +288,15 @@ func p2v(cCtx *cli.Context) error {
 	}
 	defer originFile.Close()
 	origin := &mpcsetup.Phase2{}
-	origin.ReadFrom(originFile)
+	if _, err := origin.ReadFrom(originFile); err != nil {
+		return fmt.Errorf("reading origin: %w", err)
+	}
 
-	fmt.Printf("Verifying contribution with hash: %s\n", hex.EncodeToString(computePhase2Hash(input)))
+	inputHash, err := computePhase2Hash(input)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Verifying contribution with hash: %s\n", hex.EncodeToString(inputHash))
 	if err := origin.Verify(input); err != nil {
 		fmt.Println("Phase2 Verification failed:", err.Error())
 		return err
@@ -377,7 +404,9 @@ func keys(cCtx *cli.Context) error {
 		return err
 	}
 	defer phase1File.Close()
-	phase1.ReadFrom(phase1File)
+	if _, err := phase1.ReadFrom(phase1File); err != nil {
+		return fmt.Errorf("reading phase1: %w", err)
+	}
 
 	fmt.Println("reading phase2")
 	phase2Path := cCtx.Args().Get(1)
@@ -387,7 +416,9 @@ func keys(cCtx *cli.Context) error {
 		return err
 	}
 	defer phase2File.Close()
-	phase2.ReadFrom(phase2File)
+	if _, err := phase2.ReadFrom(phase2File); err != nil {
+		return fmt.Errorf("reading phase2: %w", err)
+	}
 
 	fmt.Println("reading evals")
 	evalsPath := cCtx.Args().Get(2)
@@ -409,7 +440,9 @@ func keys(cCtx *cli.Context) error {
 		return err
 	}
 	defer r1csFile.Close()
-	r1cs.ReadFrom(r1csFile)
+	if _, err := r1cs.ReadFrom(r1csFile); err != nil {
+		return fmt.Errorf("reading r1cs: %w", err)
+	}
 
 	// Get SrsCommons from Phase1 using reflection
 	commons := getPhase1Commons(phase1)
@@ -431,9 +464,9 @@ func keys(cCtx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	err = pkTyped.WriteDump(pkFile)
-	if err != nil {
-		return err
+	defer pkFile.Close()
+	if err := pkTyped.WriteDump(pkFile); err != nil {
+		return fmt.Errorf("writing pk: %w", err)
 	}
 
 	vkFile, err := os.Create("vk")
@@ -441,7 +474,9 @@ func keys(cCtx *cli.Context) error {
 		return err
 	}
 	defer vkFile.Close()
-	vkTyped.WriteTo(vkFile)
+	if _, err := vkTyped.WriteTo(vkFile); err != nil {
+		return fmt.Errorf("writing vk: %w", err)
+	}
 
 	return nil
 }
@@ -459,7 +494,9 @@ func sol(cCtx *cli.Context) error {
 		return err
 	}
 	defer vkFile.Close()
-	vk.ReadFrom(vkFile)
+	if _, err := vk.ReadFrom(vkFile); err != nil {
+		return fmt.Errorf("reading vk: %w", err)
+	}
 
 	solFile, err := os.Create("Groth16Verifier.sol")
 	if err != nil {
